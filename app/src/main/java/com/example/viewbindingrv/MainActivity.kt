f80e5c3621adf349fba67edde61c80849c8c2620
bekach888 <beketchkoidze@gmail.com>
2023-09-08 23:57:02 +0400

    view-binding-RV

---
+++ app/src/main/java/com/example/viewbindingrv/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.viewbindingrv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import com.example.viewbindingrv.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        val adapter = MainAdapter(TaskList.taskList)
+        //binding?.taskRv?.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
+        binding?.taskRv?.adapter = adapter
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
+
+    }
+}
---
+++ app/src/main/java/com/example/viewbindingrv/TaskList.kt
@@ -0,0 +1,17 @@
+package com.example.viewbindingrv
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Take a Walk", timeStamp = "7:00 am"),
+        Task("Eat BreakFast", timeStamp = "8:00 am"),
+        Task("Attend a Meeting", timeStamp = "9:00 am"),
+        Task("Read a Book", timeStamp = "12:00 pm"),
+        Task("Eat Lunch", timeStamp = "2:00 pm"),
+        Task("Buy Groceries", timeStamp = "4:00 pm"),
+        Task("Watch Youtube Videos", timeStamp = "6:00 pm"),
+        Task("Eat dinner", timeStamp = "7:00 pm"),
+        Task("Playing Video Games", timeStamp = "8:00 pm"),
+        Task("Snack before going to bed", timeStamp = "9:00 pm"),
+        Task("Go to Bed", timeStamp = "10:00 pm")
+    )
+}
